6dc2d600cf190963aaea28289919ef7702d49074
mklasinc <mk4908@nyu.edu>
2017-10-09 11:46:36 +0400

    added category bar

--- js/scripts/CategoryBar.js
+++ js/scripts/CategoryBar.js
@@ -1,9 +1,39 @@
 function CategoryBar(tag_object){
+	var ctg_array = [];
+	create_ctg_group();
 
-	for(var ctg in tag_object){
-		console.log(ctg);
-		var ctgObj = new Category(ctg);
-    	ctgObj.createDomElement();
+	function create_ctg_group(){
+		for(var ctg in tag_object){
+			ctg_array.push(new CategoryButton(ctg));
+	    	//ctgObj.createDomElement();
+		};
+		console.log('this is our ctg array',ctg_array);
+		create_ctg_buttons();
 	};
+
+	function create_ctg_buttons(){
+		for(var i = 0; i < ctg_array.length; i++){
+			ctg_array[i].createDomElement();
+		};
+		ctg_bar_click_handler();
+	}
+
+	function ctg_bar_click_handler(){
+		var current_active_button;
+		var prev_active_button;
+		var active_ctg;
+
+		$('.ctg').click(function(e){
+			active_ctg = $(e.target).attr("id").split('-')[1];
+			console.log(active_ctg);
+			prev_active_button = current_active_button;
+			current_active_button = $(e.target);
+			$(prev_active_button).toggleClass("pink-hover toggle-click");
+			$(".tags").addClass("hidden");
+			//make the clicked object appear active
+        	$(current_active_button).toggleClass("pink-hover toggle-click");
+        	$(".tag-" + active_ctg).removeClass("hidden");
+		});
+	}
 
 }
--- js/scripts/CategoryButton.js
+++ js/scripts/CategoryButton.js
@@ -1,5 +1,5 @@
 // CATEGORY CLASS
-  function Category(selector){
+  function CategoryButton(selector){
     this.ctg = selector;
     //method to capitalize first letter of category name - used in createDomElement method
     this.capitalizeFirstLetter = function(string){
@@ -11,21 +11,7 @@
       var htmlString = '';
       htmlString += '<li><a class="ctg pink-hover"';
       htmlString += 'id=filter-' + this.ctg + '>' + this.capitalizeFirstLetter(this.ctg) + '</a></li>'; 
-      //make a variable that stores the whole object, so events can be bound to the object
-      var curObj = this;
-      //append the object to '#filters' container
-      curObj.element = $(htmlString).prependTo('#filters');
-      //bind events
-      curObj.element.click(function(e){
-        //on click get obj id
-        var objID = $(e.target).attr("id");
-        //bring all tag and category objects to default state
-        $(".ctg").removeClass("toggle-click").addClass("pink-hover");
-        $(".tags").addClass("hidden");
-        //make the clicked object appear active
-        $(e.target).toggleClass("pink-hover toggle-click");
-        $(".tag-" + curObj.ctg).removeClass("hidden");
-      });
+      $(htmlString).prependTo('#filters');
     };
     //push all the objects into an array******
     categObjArray.push(this);
