eb45977549d61dce0519aa7fb047bd31fd4922e8
mklasinc <mk4908@nyu.edu>
2017-10-16 17:00:09 +0400

    "thumbnail code cleanup"

--- js/scripts/components/Thumbnail.js
+++ js/scripts/components/Thumbnail.js
@@ -1,6 +1,6 @@
-//console.log('thumbnail class is loaded!');
 function Thumbnail(project){
-    var img_folder_path = 'img';
+
+    //project data
     this._img_url = 'img/' + project.img[0];
     this._link = project.website_url;
     this._name = project.project_name;
@@ -8,50 +8,21 @@ function Thumbnail(project){
     this._tagline = project.descr_short;
     this._semester = project.tags.semester;
     this._class = project.tags.course;
-    //console.log('new thumbnail created!');
+
+    // collection of category keys and category values
+    //for each category, attach a data property to the metadata string (to be appended to the html string)
     var tag_keys = Object.keys(project.tags);
     var tag_values = Object.values(project.tags);
-    
     var metadata_string = '';
-
-    //for each category
     tag_keys.forEach(function(e,i){
-        if(typeof e !== "object"){
-            metadata_string += 'data-' + e + '="' + tag_values[i] + '" ';
-        }else{
-            metadata_string += 'data-';
-            for(var j = 0; j < tag_values[i].length; j++){
-                metadata_string += tag_values[i][j];
-                if(j < project.custom.length - 1){
-                    metadata_string += ' ';
-                }
-            }
-        }
-        
+        metadata_string += 'data-' + e + '="' + tag_values[i] + '" ';
     });
-    //console.log(metadata_string);
-/*
-    var htmlString = '';
-    htmlString = '<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-12 thumb" ';
-    htmlString += 'data-class="' + jsonObj.course.toLowerCase() + '" ';
-    htmlString += 'data-tech="' + jsonObj.tech.toLowerCase() + '" ';
-    htmlString += 'data-custom="';
-    for(var j = 0; j < jsonObj.custom.length; j++){
-      htmlString += jsonObj.custom[j];
-      if(j < jsonObj.custom.length - 1){
-        htmlString += ' ';
-      }
-    };
-    htmlString += '">';
-    */
-    //console.log(metadata_string);
-    
 
   //create HTML elements
     var htmlString = '';
     htmlString = '<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-xs-12 thumb" ' + metadata_string + ' >';
     htmlString += '<div class="overlay-container"> ';
-    htmlString += '<img class="img-responsive" src="' + this._img_url + '" alt="">';
+    htmlString += '<div class="overlay-image-container"><img class="img-responsive" src="' + this._img_url + '" alt=""></div>';
     htmlString += '<div class="overlay-container::after"><div class="title-line"><strong>' + this._name + '</strong>  <span class="title-line-author"><br>by ' + this._author + '</span></div>';
     htmlString += '<div class="meta-line">' + this._semester + '</div></div>'; 
     htmlString += '<div class="overlay-box">';
@@ -64,32 +35,43 @@ function Thumbnail(project){
 
     $(htmlString).appendTo('.row');
 
-
-
 };
 
 //utility method for filtering data
 Thumbnail.filter = function(filterCategory,filterName){
-    console.log('filtering images!');
+
     $grid.isotope({
         filter: function() {
-          // _this_ is the item element. Get text of element's .number
-            console.log('the filter category is:', filterCategory);
-            console.log('the filter tag is: ',filterName);
+            //read the data for the target category appended to thumbnail items previously. $(this) refers to the thumbnail item (.thumb)
             var classFilter = $(this).data(filterCategory).toLowerCase();
-            console.log('the class filter is: ' + classFilter);
+            //in case the data value for a specific category is an array, split it into an array of values
             var dataArray = classFilter.split(",");
+            //display a thumbnail item if it's metadata contains a tag value that matches the filter tag
             for(var i = 0; i < dataArray.length; i++){
               if(dataArray[i] == filterName){
-                console.log('we have a match! filter: ',filterName);
-                return dataArray[i] == filterName;
-
+                return dataArray[i] == filterName; // return true to show, false to hide
             };
             
-          };
-          //console.log(classFilter);
-          // return true to show, false to hide
-          
+          };     
         }
     });
+
 };
+
+
+//DEPRECATED
+    /*tag_keys.forEach(function(e,i){
+        if(typeof e !== "object"){
+            metadata_string += 'data-' + e + '="' + tag_values[i] + '" ';
+        }else{
+            metadata_string += 'data-';
+            for(var j = 0; j < tag_values[i].length; j++){
+                console.log('this is an array value',tag_values[i][j]);
+                metadata_string += tag_values[i][j];
+                if(j < project.custom.length - 1){
+                    metadata_string += ' ';
+                }
+            }
+        }
+        
+    });*/
